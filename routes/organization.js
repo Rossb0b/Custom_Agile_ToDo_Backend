4d3a6ec8c7b09b3a3bcdbc6d8f30278cafb370ab
SofianD <doual.sofian@hotmail.com>
2020-06-11 12:31:45 +0200

    remove middlewares

--- routes/organization.js
+++ routes/organization.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const OrganizationController = require('../controllers/organization');
-const CheckMembers = require('../middlewares/organization/checkMembers');
-const CheckMethodology = require('../middlewares/organization/checkMethodology');
-const CheckBoard = require('../middlewares/organization/checkBoards');
 
 const router = express.Router();
 
 router.post(
     '',
-    CheckMembers,
-    CheckMethodology,
-    CheckBoard,
     OrganizationController.createOrganization
 );
 
