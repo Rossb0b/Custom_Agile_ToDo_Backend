16983cb14b9668324f21623c707a4cb87eb5df3f
SofianD <doual.sofian@hotmail.com>
2020-06-14 00:34:09 +0200

    create find role pipe

---
+++ pipes/organization/findRoles.js
@@ -0,0 +1,30 @@
+const Prerogative = require('../../models/organizationPrerogative');
+
+module.exports = async (roles) => {
+    try {
+        let formatedData = [];
+        if (roles.length > 0) {
+            for (let i = 0; i < roles.length; i++) {
+                let arrPrero= []
+                for(let y = 0; y < roles[i].prerogativeId.length; i++) {
+                    const result = await Prerogative.findById(roles[i].prerogativeId[y]);
+                    if (result !== null) {
+                        arrPrero.push({
+                            description: result._doc.description,
+                            index: result._doc.index
+                        });
+                    }
+                }
+                formatedData.push({
+                    _id: roles[i]._id,
+                    name: roles[i].name,
+                    prerogative: arrPrero
+                });
+            }
+        }
+        return formatedData;
+    } catch (error) {
+        // console;log('error findMembers: ', error);
+        return false;
+    }
+};
