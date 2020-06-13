bf3d66a05693a7d192466c111c054ce2cae1279e
SofianD <doual.sofian@hotmail.com>
2020-06-14 01:07:58 +0200

    fixed findBoards

--- pipes/organization/findBoards.js
+++ pipes/organization/findBoards.js
@@ -4,7 +4,7 @@ module.exports = async (boardIds) => {
     try {
         let formatedData =  [];
         if (boardIds.length > 0) {
-            for (let i = 0; i < roles.length; i++) {
+            for (let i = 0; i < boardIds.length; i++) {
                 const result = await Boards.findById(boardIds[i]);
                 if (result !== null) {
                     formatedData.push(result);
