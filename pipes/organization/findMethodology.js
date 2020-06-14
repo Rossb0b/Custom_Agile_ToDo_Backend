bf026a64a8272fefb527a9988f4b89aa24849f24
SofianD <doual.sofian@hotmail.com>
2020-06-14 12:48:08 +0200

    preparation of findMethodology

---
+++ pipes/organization/findMethodology.js
@@ -0,0 +1,19 @@
+const Methodology = require('../../models/methodology');
+
+module.exports = async (methodologyIds) => {
+    try {
+        let formatedData =  [];
+        if (methodologyIds.length > 0) {
+            for (let i = 0; i < methodologyIds.length; i++) {
+                const result = await Methodology.findById(methodologyIds[i]);
+                if (result !== null) {
+                    formatedData.push(result);
+                }
+            }
+        }
+        return formatedData;
+    } catch (error) {
+        // console;log('error findMethodology: ', error);
+        return false;
+    }
+};
