1daec3d02482360ee2d5b2915a225104267435ba
SofianD <doual.sofian@hotmail.com>
2020-06-15 14:04:03 +0200

    refacto findMembers

--- pipes/organization/findMembers.js
+++ pipes/organization/findMembers.js
@@ -2,34 +2,42 @@ const User = require('../../models/user');
 const Prerogative = require('../../models/organizationPrerogative');
 
 module.exports = async (members, roles) => {
-    try {
-        let formatedData = [];
-        for (let i = 0; i < members.length; i++) {
-            // Search user
-            const resUser = await User.findById(members[i].userId);
-            if (resUser === null) return false;
-            const {password, organization, ...formatedUser} = resUser._doc;
-            
-            // Search role
-            let role;
-            if (members[i].hasCustomRole) {
-                role = (roles.filter(x => x._id === members[i].roleId))[0];
-                role = { ...role, hasCustomRole: true };
-                
-            } else {
-                const resRole = (await Prerogative.findById(members[i].roleId))._doc;
-                if (resRole === null) return false;
-                role = { ...resRole, hasCustomRole: false };
-            }
-
-            formatedData.push({
-                user: formatedUser,
-                role: role
-            });
+    let formatedData = [];
+    for (let i = 0; i < members.length; i++) {
+        // Search user
+        let resUser;
+        try {
+            resUser = await User.findById(members[i].userId);
+        } catch (error) {
+            // console.log(error);
+            return false;
         }
-        return formatedData;
-    } catch (error) {
-        // console.log('error: ', error);
-        return false;
+        if (resUser === null) return false;
+
+        const {password, organization, ...formatedUser} = resUser._doc;
+        
+        // Search role
+        let role;
+        if (members[i].hasCustomRole) {
+            role = (roles.filter(x => x._id === members[i].roleId))[0];
+            role = { ...role, hasCustomRole: true };
+            
+        } else {
+            let resRole;
+            try {
+                resRole = (await Prerogative.findById(members[i].roleId))._doc;
+            } catch (error) {
+                return false;
+            }
+            if (resRole === null) return false;
+            role = { ...resRole, hasCustomRole: false };
+        }
+
+        formatedData.push({
+            user: formatedUser,
+            role: role
+        });
     }
+    
+    return formatedData;
 };
