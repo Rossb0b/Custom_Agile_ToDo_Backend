f56dcdd218cdb4d86e6c03c8c57f133124ec67da
SofianD <doual.sofian@hotmail.com>
2020-06-15 14:06:21 +0200

    remove durty code

--- pipes/organization/findRoles.js
+++ pipes/organization/findRoles.js
@@ -4,25 +4,6 @@ module.exports = async (roles) => {
     if (roles.length === 0 ) return [];
 
     let formatedData = [];
-
-    // try {
-    //     for (let i = 0; i < roles.length; i++) {
-    //         const result = await Prerogative.find({
-    //             '_id': { $in: roles[i].prerogativeId}
-    //         });
-    //         formatedData.push({
-    //             _id: roles[i]._id,
-    //             name: roles[i].name,
-    //             prerogative: result
-    //         });
-    //     }
-    // } catch (error) {
-    //     // console.log(error);
-    //     return false;
-    // }
-    //
-    // OU
-    //
     for (let i = 0; i < roles.length; i++) {
         let result;
         try {
