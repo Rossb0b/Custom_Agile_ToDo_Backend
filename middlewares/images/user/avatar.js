7a42cab18c21da261c666a94abb3963294e1e7ba
SofianD <doual.sofian@hotmail.com>
2020-06-18 20:25:06 +0200

    refacto

--- middlewares/images/user/avatar.js
+++ middlewares/images/user/avatar.js
@@ -2,17 +2,17 @@ const fs = require('fs-extra');
 const got = require('got');
 
 module.exports = async (req, res, next) => {
-
 	// add user image
 	if (req.body.image !== undefined) {
-		const ext = (req.body.image.src.split(';base64,'))[0].split('/').pop();
+		const dataImage = req.body.image.src.split(';base64,');
+		const ext = dataImage[0].split('/').pop();
 		if (ext.toLowerCase() !== ('png' || 'jpg' || 'jpeg')) {
 			return res.status(400).json({
 				message: 'Invalid image format',
 				ext: ext
 			});
 		}
-		const image = req.body.image.src.split(';base64,').pop();
+		const image = dataImage.pop();
 		const dateNow = Date.now();
 		const name = dateNow + '_' + req.body.image.name.toLowerCase().split(' ').join('-');
 		const imagePath = 'images/user/' + name;
@@ -45,6 +45,10 @@ module.exports = async (req, res, next) => {
 	}
 
 	const image = response.toString('base64');
+
+	//if front has an issue:
+	// image = image.split(';base64,').pop();
+
 	const dateNow = Date.now();
 	const name = dateNow + '_default_' + req.body.lastname.toLowerCase() + req.body.firstname.toLowerCase() + '.png';
 	const imagePath = 'images/user/' + name;
