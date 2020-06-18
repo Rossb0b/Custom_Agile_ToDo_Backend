cd4255b61d9690be8cf2a3a5f8b489cb42c2ca73
SofianD <doual.sofian@hotmail.com>
2020-06-18 17:26:21 +0200

    add a route to test avatar?js middleware

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const express = require('express');
 const UserController = require('../controllers/user');
 const AuthController = require('../controllers/auth');
 
+const AddAvatar = require('../middlewares/images/user/avatar');
+
 const checkAuth = require('../middlewares/auth/check-auth');
 
 const router = express.Router();
@@ -10,5 +12,8 @@ const router = express.Router();
 router.post("", UserController.createUser, AuthController.autoLogin);
 
 router.get("", checkAuth, UserController.getUserFromJWT);
+
+router.post("/avatar", AddAvatar);
+
 
 module.exports = router;
